d1bf7aab765486b5290ed278419e863cb4574068
Purav Shah <puravshah23@gmail.com>
2017-04-06 22:08:54 -0400

    remove try except

--- chirpr/views/chirps.py
+++ chirpr/views/chirps.py
@@ -12,32 +12,32 @@ chirpMod = Blueprint("chirpMod", __name__)
 def addItem():
 	error = False
 	errorMsg = ''
-	try:
-		if not session.get('loggedIn'):
-			error = True
-			errorMsg = 'Not logged in'
-			return jsonify({'status': 'error', 'error': errorMsg})
+	
+	if not session.get('loggedIn'):
+		error = True
+		errorMsg = 'Not logged in'
+		return jsonify({'status': 'error', 'error': errorMsg})
 
-		data = getRequestData(request)
+	data = getRequestData(request)
 
-		if 'content' not in data:
-			error = True
-			errorMsg = 'Invalid request'
-			return jsonify({'status': 'error', 'error': errorMsg})
+	if 'content' not in data:
+		error = True
+		errorMsg = 'Invalid request'
+		return jsonify({'status': 'error', 'error': errorMsg})
 
-		content = data['content']
-		username = session.get('username')
+	content = data['content']
+	username = session.get('username')
 
-		if (len('content') > 140):
-			return jsonify({'status':'error', 'error':'Chirp is too long'})
+	if (len('content') > 140):
+		return jsonify({'status':'error', 'error':'Chirp is too long'})
 
-		chirps = mongo.db.chirps
-		chirp = chirps.insert_one({'content': content,'username':username,'timestamp':datetime.utcnow()})
-		return jsonify({'status':'OK', 'id':str(chirp.inserted_id)})
-	except Exception as e:
-		traceback.print_exc()
-		print e
-		return jsonify({'status':'error', 'error':errorMsg})
+	chirps = mongo.db.chirps
+	chirp = chirps.insert_one({'content': content,'username':username,'timestamp':datetime.utcnow()})
+	return jsonify({'status':'OK', 'id':str(chirp.inserted_id)})
+	# except Exception as e:
+	# 	traceback.print_exc()
+	# 	print e
+	# 	return jsonify({'status':'error', 'error':errorMsg})
 
 @chirpMod.route('/item/<id>', methods=['GET'])
 def getChirp(id):
@@ -68,48 +68,47 @@ def search():
 	error = False
 	errorMsg = ''
 
-	try:
-		chirps = mongo.db.chirps
-		data = getRequestData(request)
-		print data
-		query = { '$and' : [] }
-		chirps.create_index([('content', pymongo.TEXT)])
+	chirps = mongo.db.chirps
+	data = getRequestData(request)
+	print data
+	query = { '$and' : [] }
+	chirps.create_index([('content', pymongo.TEXT)])
 
-		if not 'timestamp' in data:
-			timestamp = datetime.utcnow()
-		else:
-			timestamp = datetime.utcfromtimestamp(float(data['timestamp']))
+	if not 'timestamp' in data:
+		timestamp = datetime.utcnow()
+	else:
+		timestamp = datetime.utcfromtimestamp(float(data['timestamp']))
 
-		query["$and"].append({"timestamp" : {'$lte' : timestamp}})
+	query["$and"].append({"timestamp" : {'$lte' : timestamp}})
 
-		if 'limit' in data:
-			limit = data['limit']
-		else:
-			limit = 25
+	if 'limit' in data:
+		limit = data['limit']
+	else:
+		limit = 25
 
-		if 'q' in data:
-			searchquery = data['q']
-			query["$and"].append({"$text" : {"$search" : searchquery}})
+	if 'q' in data:
+		searchquery = data['q']
+		query["$and"].append({"$text" : {"$search" : searchquery}})
 
-		if 'username' in data:
-			username = data['username']
-			query["$and"].append({"username" : {"$eq" : username}})
+	if 'username' in data:
+		username = data['username']
+		query["$and"].append({"username" : {"$eq" : username}})
 
-		if session.get('loggedIn') is True:
-			if 'following' in data:
-				if data['following'] is True or data['following'] is None:
-					user = mongo.db.users.find_one({"username" : {"$eq" : session.get('username')}})
-					if 'following' not in user:
-						user['following'] = []
-					query["$and"].append({"following" : {"$in" : user['following']}})
-				
-		query = chirps.find(query).sort([('timestamp',-1)]).limit(limit) 
-		chirpList = []
-		for chirp in query:
-			chirp['id'] = str(chirp['_id'])
-			chirp.pop('_id', None)
-			chirpList.append(chirp)
-		return jsonify({'status':'OK', 'items':chirpList})
-	except Exception as e:
-		print traceback.print_exc()
-		return jsonify({'status':'error', 'error':errorMsg})
+	if session.get('loggedIn') is True:
+		if 'following' in data:
+			if data['following'] is True or data['following'] is None:
+				user = mongo.db.users.find_one({"username" : {"$eq" : session.get('username')}})
+				if 'following' not in user:
+					user['following'] = []
+				query["$and"].append({"following" : {"$in" : user['following']}})
+			
+	query = chirps.find(query).sort([('timestamp',-1)]).limit(limit) 
+	chirpList = []
+	for chirp in query:
+		chirp['id'] = str(chirp['_id'])
+		chirp.pop('_id', None)
+		chirpList.append(chirp)
+	return jsonify({'status':'OK', 'items':chirpList})
+	# except Exception as e:
+	# 	print traceback.print_exc()
+	# 	return jsonify({'status':'error', 'error':errorMsg})
