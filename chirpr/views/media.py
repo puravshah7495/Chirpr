7a88f11084426132576d2fe6ea68606e4e8052df
Purav Shah <puravshah23@gmail.com>
2017-04-25 18:17:25 -0400

    change media upload to gridfs

--- chirpr/views/media.py
+++ chirpr/views/media.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, request, render_template, jsonify, send_file
 from chirpr.database import getRequestData, mongo
+from werkzeug.utils import secure_filename
 import base64
 import os
+import gridfs
 
 media = Blueprint("mediaMod", __name__)
 
@@ -11,13 +13,14 @@ def addMedia():
         return render_template('media.html')
     error = False
     errorMsg = ''
-    files = mongo.db.media
-
+    # files = mongo.db.media
+    fs = gridfs.GridFS(mongo.db)
     mediaFile = request.files['content']
-    data = base64.b64encode(mediaFile.read())
-    f = files.insert_one({'content': data})
-
-    return jsonify({'status': 'OK', 'id': str(f.inserted_id)})
+    id = fs.put(mediaFile, filename=mediaFile.filename)
+    # data = base64.b64encode(mediaFile.read())
+    # f = files.insert_one({'content': data})
+    print id
+    return jsonify({'status': 'OK', 'id': str(id)})
 
 @media.route('/media/<ObjectId:id>', methods=['GET'])
 def getMedia(id):
@@ -25,11 +28,16 @@ def getMedia(id):
     error = False
     errorMsg = ''
 
-    f = files.find_one({'_id': id})
-    print id, f
-    tmp = open('tmp.png', "w")
-    tmp.write(f['content'].decode('base64'))
-    tmp.close()
-    return send_file('tmp.png', mimetype='image/png')
+    fs = gridfs.GridFS(mongo.db)
+    img = fs.get(id)
+    print img.filename
+    # f = files.find_one({'_id': id})
+    # print id, f
+    # tmp = open('tmp.png', "w")
+    # tmp.write(f['content'].decode('base64'))
+    # tmp.close()
+    response = mongo.send_file(img.filename)
+    response.headers['Content-Type'] = 'image/jpeg'
+    return response
 
 
